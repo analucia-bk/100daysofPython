4289ad4dfcd0a986054fc8b52ed2e3b3d6faa1d3
analucia-bk <analuciasolidade@gmail.com>
2022-02-21 12:57:23 -0300

    adiciona ex 3

--- Day 4/02.py
+++ Day 4/02.py
@@ -7,3 +7,4 @@ names_string = input("Give me everybody's names, separated by a comma. ")
 names = names_string.split(", ")
 random_name = names[random.randint(0, (len(names)-1))]
 print(f"{random_name} is going to buy the meal today")
+f
