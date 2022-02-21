2308a57be1ec79e43b342624d538afe292213d47
analucia-bk <analuciasolidade@gmail.com>
2022-02-21 12:20:04 -0300

    corrigindo possível bug

--- Day 4/02.py
+++ Day 4/02.py
@@ -5,6 +5,5 @@ test_seed = int(input("Create a seed number: "))
 random.seed(test_seed)
 names_string = input("Give me everybody's names, separated by a comma. ")
 names = names_string.split(", ")
-random_name = names[random.randrange(0, len(names))]
+random_name = names[random.randint(0, (len(names)-1))]
 print(f"{random_name} is going to buy the meal today")
-
